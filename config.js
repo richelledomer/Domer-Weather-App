8bbe8614096f49f88343241cabffc3a03def80f9
richelledomer <99659219+richelledomer@users.noreply.github.com>
2022-12-24 18:58:04 +0800

    update

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "caa992154ac7fbf42bfca4f5aeebf445"
+    }
+}
+
+module.exports = constants;
